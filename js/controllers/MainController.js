556f417f4ae71b9ebeb8b3befa28839b31062347
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-10 15:15:05 -0500

    stored data in scope list and rendered stories in view

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,7 @@
 app.controller('MainController', ['$scope', 'nyt', function($scope, nyt) {
-    nyt.then(response => console.log(response))
+    nyt.then((response) => {
+        $scope.list = response.map((obj) => {
+            return {title: obj.title, abstract: obj.abstract, url: obj.url};
+        })
+    });
 }]);
