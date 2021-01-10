284e57ac3e34acbd4ac961a6e88827f12e00d3d4
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-10 16:58:33 -0500

    ui styling

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,7 @@
 app.controller('MainController', ['$scope', 'nyt', function($scope, nyt) {
     nyt.then((response) => {
         $scope.list = response.map((obj) => {
-            return {title: obj.title, abstract: obj.abstract, url: obj.url};
+            return {title: obj.title, abstract: obj.abstract, url: obj.url, multimedia: obj.multimedia, published: obj.published_date};
         })
     });
 }]);
--- js/services/nyt.js
+++ js/services/nyt.js
@@ -1,6 +1,7 @@
 app.factory('nyt', ['$http', function($http) {
     return $http.get(nytApi.url, {params: {'api-key': nytApi.key}})
         .then(function(response){
+            console.log(response.data.results);
             return response.data.results;
         }, function(error){
             console.log(error);
